ba904d4726da6fbb9cfe60cf4a73f1f5ce134b42
Marc Hafner <hafner.marc@gmail.com>
2015-06-28 16:56:03 -0400

    updated images

--- examples/LINCS/CellLine_Comparison/CirclePlots/makeCytoscapeImageTable.py
+++ examples/LINCS/CellLine_Comparison/CirclePlots/makeCytoscapeImageTable.py
@@ -3,11 +3,12 @@
 import sys, os
 
 img_dir = os.getcwd()+'/img/'
+img_dir = img_dir.replace('\\','/')
 
 output_file = open('imageTableCytoscape.txt', 'w')
 output_file.write('name\tImageLoc\n')
 for img in os.listdir(img_dir):
 	img_loc = img_dir+img
 	gene_name = img.rstrip('.png')
-	output_file.write(gene_name+'\t'+'file://'+img_loc+'\n')
+	output_file.write(gene_name+'\t'+'file:///'+img_loc+'\n')
 output_file.close()
