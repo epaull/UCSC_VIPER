5aa3781507cfd9761d4acad7bcbc9b041e164ea3
epaull <evanpaull@gmail.com>
2016-02-16 10:11:16 -0800

    --unsupervised version of VIPER for CKCC analyses

---
+++ docker/viper-unsupervised/viper_unsup_wrapper.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+finish() {
+    # Fix ownership of output files
+    UID=$(stat -c '%u:%g' /data)
+    chown -R $UID /data
+}
+trap finish EXIT
+
+# Call tool with parameters
+/usr/bin/Rscript /home/UCSC_VIPER/bin/run-viper-unsupervised.R "$@" 
